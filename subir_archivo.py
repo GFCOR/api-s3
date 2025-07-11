d6c88c95b7f9485aef410ed51b03eb43f53d3465
Gianfranco Gonzalo Cordero Aguirre <145508283+GFCOR@users.noreply.github.com>
2025-07-11 04:04:35 -0500

    Update subir_archivo.py

--- subir_archivo.py
+++ subir_archivo.py
@@ -4,13 +4,19 @@ import json
 
 def lambda_handler(event, context):
     try:
-        bucket_name = event['body']['bucket']
-        file_name = event['body']['file_name']
-        file_content_base64 = event['body']['file_content']
+        # Si event['body'] es string (como ocurre normalmente), lo decodificamos
+        body = event['body']
+        if isinstance(body, str):
+            body = json.loads(body)
 
-        # Decodificamos el archivo desde Base64
+        bucket_name = body['bucket']
+        file_name = body['file_name']
+        file_content_base64 = body['file_content']
+
+        # Decodificamos el contenido del archivo
         file_content = base64.b64decode(file_content_base64)
 
+        # Subimos el archivo a S3
         s3 = boto3.client('s3')
         s3.put_object(Bucket=bucket_name, Key=file_name, Body=file_content)
 
